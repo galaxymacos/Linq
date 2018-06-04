5f132eba34fc45f6346a28c31dda8008c561712c
galaxy <galaxymacos@gmail.com>
2018-06-03 22:42:34 -0400

    + LinqDemo

--- LinqDemos/Program.cs
+++ LinqDemos/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqDemos
 {
@@ -6,11 +9,21 @@ namespace LinqDemos
     {
         public static void Main(string[] args)
         {
-            var xun = new Person("xun"," ","ruan");
-            foreach (var x in xun.Names)
+            var list = new ArrayList {1, 2, 3};
+            Console.WriteLine(list.Cast<int>().Average());
+
+            IEnumerable<int> numbers = Enumerable.Range(1, 10);
+            int[] enumerable = numbers as int[] ?? numbers.ToArray();
+            int[] arr = enumerable.ToArray();
+            List<int> arrList = enumerable.ToList();
+            Dictionary<double, bool> dict = enumerable.ToDictionary(i => (double) i / 10, i => i % 2 == 0);
+            foreach (KeyValuePair<double,bool> keyValuePair in dict)
             {
-                Console.WriteLine(x);
+                Console.WriteLine(keyValuePair.Key+" "+keyValuePair.Value);
             }
+
+            var arr2 = new[] {1, 2, 3};
+            IEnumerable<int> arre = arr2.AsEnumerable();
         }
     }
 }
---
+++ SelectAndSelectMany/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SelectAndSelectMany
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}

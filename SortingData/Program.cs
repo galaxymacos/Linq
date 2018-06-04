00c71f7701d7b729064793bbaa9a7a3fb02c4505
galaxy <galaxymacos@gmail.com>
2018-06-04 01:13:26 -0400

    + Sorting data

---
+++ SortingData/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MyExtensionLibrary;
+
+namespace SortingData
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var rand = new Random();
+            var randomValues = Enumerable.Range(1, 10).Select(x => rand.Next(11) - 5);
+            var enumerable = randomValues as int[] ?? randomValues.ToArray();
+            var csvString = new Func<IEnumerable<int>, string>(values =>
+            {
+                return string.Join(",", values.Select(v => v.ToString()).ToArray());
+            });
+            
+            Console.WriteLine(csvString(enumerable));
+            Console.WriteLine(csvString(enumerable.OrderBy(n => n)));
+            Console.WriteLine(csvString(enumerable.OrderByDescending(n => n)));            
+            
+            // Sorting class
+            var people = new List<Person>
+            {
+                new Person{Name = "Liu Yongchen",Age = 22},
+                new Person{Name = "Ruan Xun",Age = 21},
+                new Person{Name = "Ruan Xun",Age = 20},
+                new Person{Name = "He Huixian",Age = 22},
+            };
+            // Sort elements based on two criterias
+            people.OrderByDescending(p => p.Name).ThenBy(p=>p.Age).Print();
+            
+            // Reverse a string
+            string str = "abcdefg";
+            Console.WriteLine(new string(str.Reverse().ToArray()));
+        }
+    }
+}

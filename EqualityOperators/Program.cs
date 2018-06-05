183e006f573b16c5f4b4a37c3349bb4f4c5e5e38
galaxy <galaxymacos@gmail.com>
2018-06-05 01:12:31 -0400

    + Equality Operators

---
+++ EqualityOperators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EqualityOperators
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var arr1 = new[] {1, 2, 3};
+            var arr2 = new[] {1, 2, 3};
+            Console.WriteLine(arr1 == arr2);
+            Console.WriteLine(arr1.Equals(arr2));
+            Console.WriteLine(arr1.SequenceEqual(arr2));    // compare the content of two IEnumerable
+
+            var list1 = new List<int> {1, 2, 3};
+            Console.WriteLine(arr1.SequenceEqual(list1));    // We can compare an array with a list
+        }
+    }
+}

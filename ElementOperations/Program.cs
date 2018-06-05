dd49123b62b9fb4d55ea5215ffc9dda487f1755f
galaxymaxx <galaxymaxx@outlook.com>
2018-06-05 19:01:23 -0400

    + Element Operations (half donei)

---
+++ ElementOperations/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElementOperations
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new List<int> {1,1, 2, 3};
+            Console.WriteLine(numbers.First());
+            Console.WriteLine(numbers.First(x=>x>2));    // throw exception if no element satisfied this predicate
+            Console.WriteLine(numbers.Last());
+            Console.WriteLine(numbers.Last(x=>x>2));
+            Console.WriteLine(numbers.FirstOrDefault(x=>x>10));    
+            // will not throw exception; instead, return a default value
+            Console.WriteLine(numbers.Single(x=>x==1));    // 
+            Console.WriteLine(numbers.SingleOrDefault(x=>x>1));
+        }
+    }
+}

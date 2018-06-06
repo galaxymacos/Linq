88026f18b2143e526d676f5313fc2d855914b76a
galaxy <galaxymacos@gmail.com>
2018-06-05 20:11:27 -0400

    + Concatenation Operators

---
+++ ConcatenationOperators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MyExtensionLibrary;
+
+namespace ConcatenationOperators
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var integralTypes = new[] {typeof(int), typeof(short)};
+            var fpTypes = new[] {typeof(float), typeof(double)};   // concat in positive order
+            integralTypes.Concat(fpTypes).Print();    // concat in reverse order
+            integralTypes.Concat(fpTypes).Prepend(typeof(bool)).Print();
+        }
+    }
+
+    static class ExtensionMethods
+    {
+        public static IEnumerable<T> Prepend<T>(this IEnumerable<T> values, T value)
+        {
+            yield return value;
+            foreach (var item in values)
+            {
+                yield return item;
+            }
+        }
+    }
+}
